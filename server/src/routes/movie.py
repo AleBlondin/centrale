139438dbfd2424b4fbc6af5d68bacdb06b7df708
Victoire Tusseau <victoire.menu@student-cs.fr>
2019-06-17 15:34:27 +0200

    modifications

--- server/src/routes/movie.py
+++ server/src/routes/movie.py
@@ -6,7 +6,7 @@ from flask_restful import Api
 
 from resources import MovieResource
 
-USER_BLUEPRINT = Blueprint("movie", __name__)
+MOVIE_BLUEPRINT = Blueprint("movie", __name__)
 Api(MOVIE_BLUEPRINT).add_resource(
-    MovieResource, "/user/<string:name>"
+    MovieResource, "/movie/<string:name>"
 )
