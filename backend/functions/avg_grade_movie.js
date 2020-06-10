684f7bf654e2fb81380ac289d6d4b8d30a7e2879
Karle Louis-Marie <louis-marie.karle@student-cs.fr>
2020-06-10 10:11:15 +0200

    Ajout fonction back note moyenne film

---
+++ backend/functions/avg_grade_movie.js
@@ -0,0 +1,52 @@
+const DynamoDB = require('aws-sdk/clients/dynamodb');
+
+module.exports.handle = async event => {
+    if (!process.env.tableName) {
+        throw new Error('env.tableName must be defined');
+    }
+
+    const dynamoDb = new DynamoDB.DocumentClient();
+    const uuid = event.pathParameters.id;
+    var somme_note = 0;
+    var compteur = 0;
+    const result = await dynamoDb.query({
+        TableName: process.env.tableName,
+        KeyConditionExpression: '#type = :type',
+        ExpressionAttributeNames: {
+            '#type': 'type'
+        },
+        ExpressionAttributeValues: {
+            ':type': 'user',
+        },
+    }).promise();
+    const res1 = result.Items;
+    const N = res1.length;
+    for(let i = 0; i < N ; i++){
+        if(res1[i]["score"][uuid] !== undefined){
+            compteur = compteur + 1 ;
+            somme_note = somme_note + parseFloat(res1[i]["score"][uuid]);
+        }
+    }
+    
+    if (compteur !== 0){
+        const avg = somme_note/compteur;
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(avg),
+        }
+    }
+    else{
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify("Aucune Note"),
+        }
+    }
+}
